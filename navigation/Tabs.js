62704efc87b9cb601ed2a9b1e6573bc32b7c2bed
hiheecho <110316147+hiheecho@users.noreply.github.com>
2023-01-06 21:16:11 +0900

    ui:change nav,tab-bar color 네비게이션, 탭바 색을 변경합니다.

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,13 +3,30 @@ import Main from "../screen/Main";
 import MyPage from "../screen/MyPage";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useColorScheme } from "react-native";
+import {
+  DARK_BACKGROUNDCOLOR,
+  LIGHT_BACKGROUNDCOLOR,
+  BRAND_COLOR,
+  LIGHT_COLOR,
+} from "../color";
 
 const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
   const isDark = useColorScheme() === "dark";
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: isDark ? DARK_BACKGROUNDCOLOR : LIGHT_BACKGROUNDCOLOR,
+      }}
+      screenOptions={{
+        headerTintColor: "#fff",
+        headerStyle: { backgroundColor: BRAND_COLOR },
+        tabBarStyle: { backgroundColor: BRAND_COLOR },
+        tabBarActiveTintColor: "#FFF",
+        tabBarInactiveTintColor: "#FFF",
+      }}
+    >
       <Tab.Screen name="Main" component={Main} />
       <Tab.Screen name="MyPage" component={MyPage} />
     </Tab.Navigator>
