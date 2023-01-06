99958e7572706396bab44ccf7bfb2adb79452a35
cinephile <aprilkb412@naver.com>
2023-01-06 22:27:03 +0900

    ui: Post 구현 완료

--- screen/Main.jsx
+++ screen/Main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Main() {
+const Main = () => {
+  const { navigate } = useNavigation();
+
   return (
     <View>
       <Text>Main</Text>
+      <TouchableOpacity onPress={() => navigate("Stacks", { screen: "Post" })}>
+        <Text>임시 글작성 버튼</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+export default Main;
