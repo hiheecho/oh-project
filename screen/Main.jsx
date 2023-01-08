230e7fde441fb102ee0508563172394d8c5d07e5
hiheecho <110316147+hiheecho@users.noreply.github.com>
2023-01-08 23:53:44 +0900

    feat:edit test code
    test용 code를 주석처리합니다.
    
    @hiheecho

--- screen/Main.jsx
+++ screen/Main.jsx
@@ -3,11 +3,11 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 //여기부터 테스트용 코드
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
-import { dbService } from "../firebase";
-import ReviewList from "../components/ReviewList";
+// import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+// import { dbService } from "../firebase";
+// import ReviewList from "../components/ReviewList";
 //여기까지
 
 const Main = () => {
@@ -15,22 +15,22 @@ const Main = () => {
 
   //여기부터 테스트용 코드
 
-  const [reviews, setReviews] = useState([]);
+  // const [reviews, setReviews] = useState([]);
 
-  useEffect(() => {
-    const q = query(
-      collection(dbService, "contents"),
-      orderBy("createdAt", "desc")
-    );
-    const unsubscribe = onSnapshot(q, (snapshot) => {
-      const newReviews = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setReviews(newReviews);
-    });
-    return unsubscribe;
-  }, []);
+  // useEffect(() => {
+  //   const q = query(
+  //     collection(dbService, "contents"),
+  //     orderBy("createdAt", "desc")
+  //   );
+  //   const unsubscribe = onSnapshot(q, (snapshot) => {
+  //     const newReviews = snapshot.docs.map((doc) => ({
+  //       id: doc.id,
+  //       ...doc.data(),
+  //     }));
+  //     setReviews(newReviews);
+  //   });
+  //   return unsubscribe;
+  // }, []);
 
   //여기까지
 
@@ -41,9 +41,9 @@ const Main = () => {
         <Text>임시 글작성 버튼</Text>
       </TouchableOpacity>
       {/* text용 List 임시컴포넌트 */}
-      {reviews.map((review) => {
+      {/* {reviews.map((review) => {
         return <ReviewList key={review.id} review={review} />;
-      })}
+      })} */}
 
       {/* 여기까지 */}
     </View>
