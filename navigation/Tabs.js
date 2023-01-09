cefd9da80ed5097bf2f7a01bcc1f50d430cd5c4e
park74441 <118245133+park74441@users.noreply.github.com>
2023-01-09 21:17:12 +0900

    login 수정 완료 및 mypage 임시 로그아웃 버튼

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Main from "../screen/Main";
 import MyPage from "../screen/MyPage";
+import Login from "../screen/Login";
+import { useState, useEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useColorScheme } from "react-native";
 import {
@@ -10,10 +12,22 @@ import {
   LIGHT_COLOR,
 } from "../color";
 import { Ionicons } from "@expo/vector-icons";
+import { auth } from "../firebase";
 
 const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
+  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+  });
+
   const isDark = useColorScheme() === "dark";
   return (
     <Tab.Navigator
@@ -28,32 +42,45 @@ export default function Tabs() {
         tabBarInactiveTintColor: "#FFF",
       }}
     >
-      <Tab.Screen
-        name="Main"
-        component={Main}
-        options={{
-          tabBarLabel: "Home",
-          tabBarIcon: ({ focused }) =>
-            focused ? (
-              <Ionicons name="md-home-sharp" size={24} color="white" />
-            ) : (
-              <Ionicons name="md-home-outline" size={24} color="white" />
-            ),
-        }}
-      />
-      <Tab.Screen
-        name="MyPage"
-        component={MyPage}
-        options={{
-          tabBarLabel: "My",
-          tabBarIcon: ({ focused }) =>
-            focused ? (
-              <Ionicons name="md-person" size={24} color="white" />
-            ) : (
-              <Ionicons name="md-person-outline" size={24} color="white" />
-            ),
-        }}
-      />
+      {isLoggedIn ? (
+        <Tab.Screen
+          name="Main"
+          component={Main}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="md-home-sharp" size={24} color="white" />
+              ) : (
+                <Ionicons name="md-home-outline" size={24} color="white" />
+              ),
+          }}
+        />
+      ) : (
+        <Tab.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+            tabBarStyle: { display: "none" },
+          }}
+        />
+      )}
+      {isLoggedIn ? (
+        <Tab.Screen
+          name="MyPage"
+          component={MyPage}
+          options={{
+            tabBarLabel: "My",
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="md-person" size={24} color="white" />
+              ) : (
+                <Ionicons name="md-person-outline" size={24} color="white" />
+              ),
+          }}
+        />
+      ) : null}
     </Tab.Navigator>
   );
 }
