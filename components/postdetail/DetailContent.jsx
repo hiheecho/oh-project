089d8a9d7f65da81c5d1c4cb8cd8c575659ecb08
cinephile <aprilkb412@naver.com>
2023-01-12 20:42:50 +0900

    ui: main 카드 텍스트 길이 변경

--- components/postdetail/DetailContent.jsx
+++ components/postdetail/DetailContent.jsx
@@ -50,7 +50,7 @@ const DetailContent = ({ item }) => {
       <ContentHeader>
         <UserInfo>
           {_maybeRenderImage(item)}
-          <Nickname>{item.userName}</Nickname>
+          <Nickname>{data?.data().userName}</Nickname>
         </UserInfo>
       </ContentHeader>
       <Line />
